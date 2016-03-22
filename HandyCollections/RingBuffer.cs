e0342d0ca9f2a6ccdd245fbb55031edfc735521f
Martin Evans <martindevans@gmail.com>
2016-03-22 15:23:09 +0000

    And this time, I actually added the new files :|
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ HandyCollections/RingBuffer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HandyCollections
+{
+    /// <summary>
+    /// Stores the N most recently added items
+    /// </summary>
+    public class RingBuffer<T>
+        : IEnumerable<T>
+    {
+        private readonly T[] _items;
+
+        /// <summary>
+        /// Indicates the number of items added to the collection and currently stored
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// The size of this ring buffer
+        /// </summary>
+        public int Capacity => _items.Length;
+
+        private int _end;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                if (Count < Capacity)
+                    return _items[index];
+                return _items[(_end + index) % _items.Length];
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="size"></param>
+        public RingBuffer(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentOutOfRangeException(nameof(size), "Capacity must be > 0");
+
+            _items = new T[size];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            _items[_end] = item;
+            _end = (_end + 1) % _items.Length;
+
+            if (Count < _items.Length)
+                Count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < Count; i++)
+                yield return this[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ HandyCollectionsTest/RingBufferTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandyCollections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HandyCollectionsTest
+{
+    [TestClass]
+    public class RingBufferTest
+    {
+        [TestMethod]
+        public void AssertThat_ConstructingRingBuffer_DoesNotThrow()
+        {
+            var r = new RingBuffer<int>(5);
+
+            Assert.AreEqual(0, r.Count);
+            Assert.AreEqual(5, r.Capacity);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AssertThat_ConstructingWithNegativeCapacity_Throws()
+        {
+            var r = new RingBuffer<int>(-5);
+        }
+
+        [TestMethod]
+        public void AssertThat_AddingToRingBuffer_AddsItems()
+        {
+            var r = new RingBuffer<int>(5)
+            {
+                1,
+                2
+            };
+
+
+            Assert.AreEqual(1, r[0]);
+            Assert.AreEqual(2, r[1]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AssertThat_IndexingAfterCount_Throws()
+        {
+            var r = new RingBuffer<int>(5)
+            {
+                1,
+                2
+            };
+
+
+            var v = r[3];
+        }
+
+        [TestMethod]
+        public void AssertThat_AddingMoreThanCapacity_RemovesOldestValue()
+        {
+            var r = new RingBuffer<int>(5);
+            var l = new List<int>();
+
+            //We'll use the tail of a list to emulate a ringbuffer and check that the actual ringbuffer always contains the same values
+
+            for (var i = 0; i < 1000; i++)
+            {
+                //Add to list - last 5 items in list should be items in ringbuffer
+                l.Add(i);
+
+                //Add to ring buffer
+                r.Add(i);
+
+                //Ensure two buffers are exactly the same
+                for (int j = 0; j < r.Count; j++)
+                {
+                    Assert.AreEqual(l[l.Count - r.Count + j], r[j]);
+                }
+            }
+        }
+    }
+}
