12d6a8f96b86fb91640a4f911733621ab1eed95c
Martin Evans <martindevans@gmail.com>
2016-09-28 19:21:46 +0100

    Added "Source" property to nuget push command
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- push-all.ps1
+++ push-all.ps1
@@ -1,12 +1,12 @@
 function Push-Nuget($path, $csproj) {
     $fullPathToCsprog = Join-Path -Path $path -ChildPath $csproj -resolve;
     
-    nuget pack $fullPathToCsprog -Prop Configuration=Release -IncludeReferencedProjects
+    nuget pack $fullPathToCsprog -Prop Configuration=Release -IncludeReferencedProjects -Symbols
     
     get-childitem -Filter *.nupkg -name | foreach ($_) {
         Write-Host "Pushing " $_ -backgroundcolor darkgreen -foregroundcolor white;
     
-        nuget push $_
+        nuget push $_ -Source https://www.nuget.org/api/v2/package
         Remove-Item $_
         
         Write-Host "Done " $_ -backgroundcolor darkgreen -foregroundcolor white;
